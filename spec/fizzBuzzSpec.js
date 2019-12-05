f142c3cd915c904cd10f5c58562f909e25a5962f
Anh Nguyen <anhnguyenis@gmail.com>
2019-12-05 15:18:33 +0000

    add tests for fizzbuzz number divisible by 5

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,17 +1,31 @@
 describe('Fizzbuzz', function() {
   var fizzbuzz;
 
+  beforeEach(function() {
+    fizzbuzz = new Fizzbuzz();
+  });
+
   describe('knows when a number is', function(){
     it('divisible by 3', function(){
-      fizzbuzz = new Fizzbuzz();
       expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
     });
   });
 
   describe('knows when a number is NOT', function(){
     it('divisible by 3', function(){
-      fizzbuzz = new Fizzbuzz();
       expect(fizzbuzz.isDivisibleByThree(1)).toBe(false);
+    });
+  });
+
+  describe('knows when a number is', function(){
+    it('divisible by 5', function(){
+      expect(fizzbuzz.isDivisibleByFive(5)).toBe(true);
+    });
+  });
+
+  describe('knows when a number is NOT', function(){
+    it('divisible by 5', function(){
+      expect(fizzbuzz.isDivisibleByFive(1)).toBe(false);
     });
   });
 
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -3,3 +3,7 @@ var Fizzbuzz = function () {};
 Fizzbuzz.prototype.isDivisibleByThree = function(number) {
   return (number % 3 === 0);
 };
+
+Fizzbuzz.prototype.isDivisibleByFive = function(number) {
+  return (number % 5 === 0);
+};
